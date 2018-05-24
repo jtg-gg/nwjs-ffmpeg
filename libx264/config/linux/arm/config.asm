a329efe31c34e272a94f3f1b15090b4edbfc3164
Jefry <jefry.ted@vcube.co.jp>
2018-05-24 11:56:28 +0700

    update x264 to "latest" vesion, add nasm toolchain

--- libx264/config/linux/arm/config.asm
+++ libx264/config/linux/arm/config.asm
@@ -1,7 +1,8 @@
 %define HAVE_MALLOC_H 1
+%define HAVE_ARM_INLINE_ASM 1
 %define ARCH_ARM 1
 %define SYS_LINUX 1
-%define STACK_ALIGNMENT 16
+%define STACK_ALIGNMENT 4
 %define HAVE_POSIXTHREAD 1
 %define HAVE_CPU_COUNT 0
 %define HAVE_THREAD 1
@@ -17,9 +18,9 @@
 %define HAVE_ALTIVEC 0
 %define HAVE_ALTIVEC_H 0
 %define HAVE_MMX 0
-%define HAVE_ARMV6 0
-%define HAVE_ARMV6T2 0
-%define HAVE_NEON 0
+%define HAVE_ARMV6 1
+%define HAVE_ARMV6T2 1
+%define HAVE_NEON 1
 %define HAVE_BEOSTHREAD 0
 %define HAVE_WIN32THREAD 0
 %define HAVE_SWSCALE 0
